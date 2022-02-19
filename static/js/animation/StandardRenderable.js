71b7355f7234df19311c0f727357e27d1cbc4aca
piotriwaniuk <piekoszek@gmail.com>
2022-02-19 19:11:54 +0100

    nie potrzebuje ja tych wiadomosci

--- static/js/animation/StandardRenderable.js
+++ static/js/animation/StandardRenderable.js
@@ -2,8 +2,14 @@ import AnimatedRenderable from "./AnimatedRenderable.js";
 
 class StandardRenderable extends AnimatedRenderable {
 
-    constructor(x = 0, y = 0) {
+    passColor = true;
+
+    constructor(x = 0, y = 0, passColor = "true") {
         super();
+
+        if (passColor === "false") {
+            this.passColor = false;
+        }
 
         this.setPosition(x, y);
     }
@@ -14,9 +20,10 @@ class StandardRenderable extends AnimatedRenderable {
 
     setColor(r = 1, g = 1, b = 1, a = 1) {
         super.setColor(r, g, b, a);
-        this.children.forEach(c => c.setColor(r, g, b, a));
+        if (this.passColor) {
+            this.children.forEach(c => c.setColor(r, g, b, a));
+        }
     }
-
 }
 
 export default StandardRenderable;
